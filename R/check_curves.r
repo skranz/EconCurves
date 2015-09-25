4d7a4acec41443c633db36b4a87ca1c714801c62
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2015-09-25 17:28:39 +0200

    update

--- R/check_curves.r
+++ R/check_curves.r
@@ -4,9 +4,11 @@ missing.fields = function(li, fields) {
 }
 
 check.curve = function(curve) {
+  restore.point("check.curve")
+
   missing = missing.fields(curve, c("xvar","yvar","name","eq","color"))
   if (length(missing)>0) {
     stop(paste0("You curve has not specified the required fields ", paste0(missing, collapse=", "),"."))
   }
-  
+
 }
