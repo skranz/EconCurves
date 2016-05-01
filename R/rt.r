0d8b3081f61f212fa7eafbe06fc0fd78d0b35557
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2016-05-01 13:59:00 +0200

    updates

--- R/rt.r
+++ R/rt.r
@@ -136,7 +136,7 @@ panequiz.parse.fun = function(inner.txt,type="panequiz",name=args$name,id=paste0
   for (i in seq_along(steps)) {
     step = parse.hashdot.yaml(txt[(lines[i]+1):(lines[i+1]-1)])
     step$step.num = i
-    step = make.panequiz.step(step, given_show=given_show, pane=ao$pane,ps=ps,bi=bi)
+    step = panequiz.make.step(step, given_show=given_show, pane=ao$pane,ps=ps,bi=bi)
     steps[[i]] = step
     given_show = step$postshow      
   }
@@ -158,16 +158,19 @@ panequiz.parse.fun = function(inner.txt,type="panequiz",name=args$name,id=paste0
   
   textOutput = as.character(uiOutput(ao$textId))
   paneOutput = as.character(uiOutput(ao$paneId))  
-  ao$layout = HTML(replace.whiskers(ao$layout, list(text=textOutput,pane=paneOutput)))
+  menu = as.character(panequiz.menu.bar(bi=bi))
+  ao$layout = HTML(replace.whiskers(ao$layout, list(text=textOutput,pane=paneOutput,menu=menu)))
   
   
   return(ao)
 }
 
-panequiz.ui.fun = function(ts,...) {
+panequiz.ui.fun = function(ts,bi,...) {
   restore.point("pane.quiz.ui.fun")
   ao = ts$ao
   ui = ao$layout
+  
+
   panequiz.show.step(ts=ts)
   ui
 }
@@ -193,31 +196,40 @@ panequiz.show.step = function(step.num=ts$step.num, step=ao$steps[[step.num]], a
   setUI(ao$paneId,plot.html)  
 }
 
-panequiz.show.next.step = function(ts) {
+panequiz.show.next.step = function(ts,...) {
   restore.point("panequiz.show.next.step")
-  next.step.num = ts$step.num +1
-  if (next.step.num > length(ts$ao$steps)) return()
   
-  if (next.step.num > ts$sts$steps.solved +1) {
-    panequiz.show.step(ts,msg="You must solve the current task, before you can see the next task.")
+  # first move to success
+  if (ts$step.mode == "pre" | ts$step.mode == "failure") {
+    if (ts$step.num > ts$sts$steps.solved) {
+      panequiz.show.step(ts=ts,msg="You must solve the current task, before you can see the next task.")
+    }
+    ts$step.mode = "success"
+    panequiz.show.step(ts=ts)
     return()
   }
+    
+  next.step.num = ts$step.num +1
+  if (next.step.num > length(ts$ao$steps))
+     return()
+  
   ts$step.num = next.step.num
   ts$step.mode = "pre"
   panequiz.show.step(ts=ts)
 }
 
-panequiz.show.prev.step = function(ts=NULL, next.step.mode="pre") {
+panequiz.show.prev.step = function(ts, next.step.mode="pre",...) {
   restore.point("panequiz.show.prev.step")
   next.step.num = max(ts$step.num -1,1)
+  ts$step.num = next.step.num
   ts$step.mode = next.step.mode   
   panequiz.show.step(ts=ts)
 }
 
 
 
-make.panequiz.step = function(step, pane, given_shows,ps,bi) {
-  restore.point("make.panequiz.step")
+panequiz.make.step = function(step, pane, given_shows,ps,bi) {
+  restore.point("panequiz.make.step")
   
   if (is.null(step$points)) step$points = 0
   if (is.null(step$score)) step$score = 0
@@ -241,13 +253,16 @@ make.panequiz.step = function(step, pane, given_shows,ps,bi) {
   
   step$post.plot = res 
   
-  pre.html = md2html(step$tell)
+  whisker.values = whisker.values.from.data_rows(data=pane$data, data_rows=pane$data_rows)
+  
+  pre.html = replace.whiskers(md2html(step$tell), whisker.values)
+  success.html = replace.whiskers(md2html(step$success), whisker.values)
+  if (is.null(step$failure)) step$failure = "---\nNot yet correct!"
+  failure.html = replace.whiskers(md2html(step$failure), whisker.values)
   
   step$pre.html = withMathJax(HTML(pre.html))
-  step$success.html = withMathJax(HTML(paste0(pre.html, md2html(step$success))))
-  if (is.null(step$failure)) step$failure = ""
-  step$failure.html = withMathJax(HTML(paste0(pre.html, md2html(step$failure))))
-  
+  step$success.html = withMathJax(HTML(paste0(pre.html, success.html)))
+  step$failure.html = withMathJax(HTML(paste0(pre.html,failure.html)))
   
   step = panequiz.make.step.task(step=step, pane=pane, ps=ps) 
   
@@ -258,16 +273,57 @@ panequiz.make.step.task = function(step,pane,ps) {
   restore.point("panequiz.make.step.task")
 
   task.fields = c("find","find_shift")
-  step$task.type = task.fields[task.fields %in% names(step)]
+  step$task.type = task.type = task.fields[task.fields %in% names(step)]
   if (length(step$task.type)==0) step$task.type = "none"
   step$has.task = step$task.type != "none"
   if (!step$has.task) return(step)
   
-  step$task.fun = function(xy,...) {
-    list(ok=TRUE)
+
+  task.fun.env = new.env(parent=globalenv())
+
+  # find geom that are specified in the task
+  nr = get.geom.name.and.row(step[[task.type]],pane$data_rows, data=pane$data)
+  if (task.type == "find") {
+    restore.point("make.find.task")
+    
+    task.fun.env$geom = pane$geoms.li[[nr$row]][[nr$name]]
+    task.fun = function(xy,...) {
+      ok = is.point.on.geom(xy,geom)
+      list(ok=ok)
+    }
+  } else if (task.type == "find_shift") {
+    restore.point("make.find.shift.task")
+    
+    if (NROW(nr)==1) {
+      if (nr$row != data_rows[1]) stop("If you specify only a single symbol name in find_shift it must correspond to the first data_row")
+      
+      source.geom = pane$geoms.li[[data_rows[1]]][[nr$name]]
+      dest.geom = pane$geoms.li[[data_rows[2]]][[nr$name]]
+      
+    } else {
+      source.geom = pane$geoms.li[[nr$row[1] ]][[nr$name[1] ]]
+      dest.geom = pane$geoms.li[[nr$row[2] ]][[nr$name[2] ]]
+    }
+    ggpos = geom.to.geom.pos(new=dest.geom, old=source.geom)
+    
+    task.fun.env$source.geom = source.geom
+    task.fun.env$ggpos = ggpos
+    task.fun = function(xy,...) {
+      pgpos = point.to.geom.pos(xy, source.geom)
+      ok = length(setdiff(ggpos,pgpos))==0
+      list(ok=ok)
+    }
+  } else {
+    task.fun = function(xy,...) {
+      list(ok=TRUE)
+    }
   }
+  environment(task.fun) = task.fun.env
+  step$task.fun = task.fun
   return(step)
 }
+
+
 
 panequiz.make.org.task.state = function(ao,...) {
   list(
@@ -309,15 +365,22 @@ panequiz.shown.txt.fun = function(ts,solved=FALSE,...) {
   "\n--A pane quiz--\n"
 } 
 
-panequiz.sol.txt.fun = function(ts,solved=TRUE,...) {
+panequiz.sol.txt.fun = panequiz.out.txt.fun = function(ts,solved=TRUE,...) {
   "\n--A pane quiz--\n"
 }
 
-panequiz.init.handlers = function(ao=ts$ao,ps=get.ps(), app=getApp(),ts=NULL,...) {
+panequiz.init.handlers = function(ao=ts$ao,ps=get.ps(), app=getApp(),ts=NULL,bi,...) {
   restore.point("panequiz.init.handlers")
   
   pane = ao$pane
   imageClickHandler(pane$img.id,fun = panequiz.click,ts=ts,pane=pane)  
+  
+  nextBtnId = paste0("panequizNextBtn_",bi)
+  prevBtnId = paste0("panequizPrevBtn_",bi)
+  buttonHandler(nextBtnId, panequiz.show.next.step, ts=ts)
+  buttonHandler(prevBtnId, panequiz.show.prev.step, ts=ts)
+  
+  
 }
 
 panequiz.click = function(x,y,...,ts=NULL,pane=NULL) {
@@ -337,7 +400,7 @@ panequiz.click = function(x,y,...,ts=NULL,pane=NULL) {
   # translate pixel to plot coordinates
   coordmap = step$pre.plot$coordmap
   px = x; py = y
-  xy = scaleInvCoords(px,py,coordmap)
+  xy = unlist(scaleInvCoords(px,py,coordmap))
   
   res = step$task.fun(xy=xy)
   
@@ -359,15 +422,72 @@ panequiz.click = function(x,y,...,ts=NULL,pane=NULL) {
   cat("\npixel = ",c(px,py)," xy = ",unlist(xy))
 }
 
-panequiz.menu.bar = function(ao) {
+panequiz.menu.bar = function(bi) {
+  restore.point("panequiz.menu.bar")
+  
+  nextBtnId = paste0("panequizNextBtn_",bi)
+  prevBtnId = paste0("panequizPrevBtn_",bi)
+  
   div(class="panequiz-menu-bar",
     HTML("<table width='100%'><tr>"),
-    HTML("<td align='right' valign='center' nowrap>"),
+    HTML("<td align='left' valign='center' nowrap>"),
     tagList(
-      bsButton("panequizPrevBtn","<",size = "extra-small"),
-      bsButton("panequizNextBtn",">",size = "extra-small"),
+      bsButton(prevBtnId,"<",size = "extra-small"),
+      bsButton(nextBtnId,">",size = "extra-small")
     ),
     HTML("</td></tr></table>")
   )  
+  
+}
 
+
+get.geom.name.and.row = function(id,data_rows=1, all.cols = names(data), data=NULL) {
+  restore.point("get.geom.name.and.row")
+  
+  role.names = names(data_rows)
+  if (is.null(role.names))
+    role.names = as.character(data_rows)
+
+  res.df = data_frame(name=id, row=data_rows[1])
+  
+  for (i in seq_along(role.names)) {
+    rn = role.names[i]
+    rcols = paste0(all.cols,"_",rn)
+    rows = match(id,rcols)
+    wrows = which(!is.na(rows))
+    if (length(wrows)>0) {
+      res.df$name[wrows] = all.cols[rows[wrows]]
+      res.df$row[wrows] = data_rows[i]
+    }
+  }
+  no.match = !res.df$name %in% all.cols
+  if (any(no.match)) {
+    stop("Could not find the field(s) ", paste(res.df$name[no.match], collapse=", ")," in the data.")
+  }
+  res.df 
+}
+whisker.values.from.data_rows = function(data, data_rows) {
+  restore.point("whisker.values.from.data_rows")
+  
+  role.names = names(data_rows)
+  if (is.null(role.names))
+    role.names = as.character(data_rows)
+  all.cols = colnames(data)
+  val0 = as.list(data[data_rows[1],])
+  val.li = lapply(seq_along(data_rows), function(i) {
+    vals = as.list(data[data_rows[i],])
+    names(vals) = paste0(all.cols,"_", role.names[i])
+    vals
+  })
+  c(val0, do.call(c,val.li))
+   
+  
+}
+
+
+
+replace.data_rows.whiskers = function(txt, data=pane$data, data_rows=pane$data_rows, pane=NULL) {
+  restore.point("panequiz.replace.whiskers")
+  values = whisker.values.from.data_rows(data=data, data_rows=data_rows)
+  replace.whiskers(txt, values)
 }
